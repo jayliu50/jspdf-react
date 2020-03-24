65f6ce0ab347a0439b7953c541dc83ee236316cd
Stefan <stefan@poweredbycoffee.co.uk>
2020-03-24 18:52:03 +0000

    adding html settings, callback, margins

--- src/Html.js
+++ src/Html.js
@@ -7,13 +7,16 @@ const Html = (props) => {
     x,
     y,
     sourceById,
-    importHtml
+    importHtml,
+    settings = null,
+    callback = null,
+    margins = null
   } = props
   return (
     <Consumer>
       {(context) => {
         const html = sourceById ? document.getElementById(sourceById) : importHtml
-        context.doc.fromHTML(html.innerHTML, x, y)
+        context.doc.fromHTML(html.innerHTML, x, y, settings, callback, margins)
         return context.addProperty(context.doc)
       }}
     </Consumer>
